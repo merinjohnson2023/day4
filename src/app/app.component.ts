aae06a3c7d26422db7a0a56221c8f4332ef8e8b0
merinjohnson2023 <153511617+merinjohnson2023@users.noreply.github.com>
2023-12-18 16:19:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './units/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DistrictComponent } from './district/district.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './units/footer/footer.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NavbarComponent,HomeComponent,DistrictComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DistrictComponent } from './district/district.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+{
+    path:'',component:HomeComponent
+},
+
+{
+    path:'district',component:DistrictComponent
+},
+
+{
+    path:'contact',component:ContactComponent
+},
+
+
+];
